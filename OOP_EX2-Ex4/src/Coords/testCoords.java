f4372e5fd91a93c0d46c9e8b7e2c7b78862765d1
admin <admin@DESKTOP-BUNN6F5>
2018-11-29 14:29:21 +0200

    ...

--- OOP_EX2-Ex4/src/Coords/testCoords.java
+++ OOP_EX2-Ex4/src/Coords/testCoords.java
@@ -8,12 +8,12 @@ public class testCoords {
 		MyCoords m = new MyCoords();
 		Point3D p1 = new Point3D(32.103315,35.209039,670);
 		Point3D p2 = new Point3D(32.106352,35.205225,650);
-		System.out.println("distance: "+m.distance3d(p2, p1));
+		System.out.println("Distance: "+m.distance3d(p2, p1));
 		Point3D p3 = new Point3D(m.vector3D(p1, p2));
-		System.out.println("vector between: "+m.vector3D(p1, p2));
-		//System.out.println(p3);
-		System.out.println("add vector: "+m.add(p1, p3));
-		System.out.println("cheack: "+m.isValid_GPS_Point(p1));
+		System.out.println("Vector between: "+m.vector3D(p1, p2));
+		System.out.println(p3);
+		System.out.println("Add Vector: "+m.add(p1, p3));
+		System.out.println("Cheack: "+m.isValid_GPS_Point(p1));
 	}
 
 }
