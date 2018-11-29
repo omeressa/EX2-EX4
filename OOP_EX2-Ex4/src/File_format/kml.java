04dceb2e32ab79497f6c4d49041edec992cea5fb
admin <admin@DESKTOP-BUNN6F5>
2018-11-29 15:58:28 +0200

    kml file updated

--- OOP_EX2-Ex4/src/File_format/kml.java
+++ OOP_EX2-Ex4/src/File_format/kml.java
@@ -29,21 +29,61 @@ public class kml {
     }
 
     public void toKmlFile(String path) throws FileNotFoundException {
-    	//private static String KmlFileTemplateFormat = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document>\n%s\n</Document>\n</kml>";
-    	//private static String KmlPlacemarkTemplateFormat = "<Placemark>\n<name>%s</name>\n<TimeStamp>\n<when>%s</when>\n</TimeStamp>\n<description>%s</description>\n<Point>\n<coordinates>%s,%s,%s</coordinates></Point>\n</Placemark>";
-    	String KmlFileTemplateFormat= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<kml xmlns=\\\"http://www.opengis.net/kml/2.2\\\">\n<Document>\n<Folder>\n<name/>";
+    	/*
+    	PrintWriter writer = new PrintWriter(new File(path));
+        writer.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        writer.println("<kml xmlns=\"http://www.opengis.net/kml/2.2\">");
+        writer.println("<Document>");
+        writer.println("<Folder>");
+        writer.println("<name/>");
         for (GIS_element element : points) {
-        	KmlFileTemplateFormat+="<Placemark>";
-        	KmlFileTemplateFormat+="<name/>";
+            writer.println("<Placemark>");
+            writer.println("<name/>");
             Date date = new Date(element.getData().getUTC());
             DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            KmlFileTemplateFormat+="<description><![CDATA[Timestamp: <b>" + element.getData().getUTC() + "</b><br/>Date: <b>" + df.format(date) + "</b>]]></description>";
-            KmlFileTemplateFormat+="<Point>";
-            KmlFileTemplateFormat+="<coordinates>" + ((Point3D) element.getGeom()).y() + "," + ((Point3D) element.getGeom()).x() + "," + ((Point3D) element.getGeom()).z() + "</coordinates>";
-            KmlFileTemplateFormat+="</Point>";
-            KmlFileTemplateFormat+="</Placemark>";	
+            writer.println("<description><![CDATA[Timestamp: <b>" + element.getData().getUTC() + "</b><br/>Date: <b>" + df.format(date) + "</b>]]></description>");
+            writer.println("<Point>");
+            writer.println("<coordinates>" + ((Point3D) element.getGeom()).y() + "," + ((Point3D) element.getGeom()).x() + "," + ((Point3D) element.getGeom()).z() + "</coordinates>");
+            writer.println("</Point>");
+            writer.println("</Placemark>");
         }
-        KmlFileTemplateFormat+="</Folder>\n</Document>\n</kml>";
-        System.out.print(KmlFileTemplateFormat);       
+        writer.println("</Folder>");
+        writer.println("</Document>");
+        writer.println("</kml>");
+        writer.close();
+    }
+    	 */
+    	//private static String KmlFileTemplateFormat = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document>\n%s\n</Document>\n</kml>";
+    	//private static String KmlPlacemarkTemplateFormat = "<Placemark>\n<name>%s</name>\n<TimeStamp>\n<when>%s</when>\n</TimeStamp>\n<description>%s</description>\n<Point>\n<coordinates>%s,%s,%s</coordinates></Point>\n</Placemark>";
+    	
+		String fileName = "output.kml";
+		PrintWriter PrintWriter = null;
+		try 
+		{
+			PrintWriter = new PrintWriter(new File(fileName));
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+			return;
+		}
+    	StringBuilder KmlFileTemplateFormat = new StringBuilder();
+    	 KmlFileTemplateFormat.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<kml xmlns=\\\"http://www.opengis.net/kml/2.2\\\">\n<Document>\n<Folder>\n<name/>");
+    	//String KmlFileTemplateFormat= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<kml xmlns=\\\"http://www.opengis.net/kml/2.2\\\">\n<Document>\n<Folder>\n<name/>";
+        for (GIS_element element : points) {
+        	KmlFileTemplateFormat.append("<Placemark>");
+        	KmlFileTemplateFormat.append("<name/>");
+            Date date = new Date(element.getData().getUTC());
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            KmlFileTemplateFormat.append("<description><![CDATA[Timestamp: <b>" + element.getData().getUTC() + "</b><br/>Date: <b>" + df.format(date) + "</b>]]></description>");
+            KmlFileTemplateFormat.append("<Point>");
+            KmlFileTemplateFormat.append("<coordinates>" + ((Point3D) element.getGeom()).y() + "," + ((Point3D) element.getGeom()).x() + "," + ((Point3D) element.getGeom()).z() + "</coordinates>");
+            KmlFileTemplateFormat.append("</Point>");
+            KmlFileTemplateFormat.append("</Placemark>");	
+        }
+        PrintWriter.write(KmlFileTemplateFormat.toString());
+        PrintWriter.close();
+     //   KmlFileTemplateFormat+="</Folder>\n</Document>\n</kml>";
+     //   System.out.print(KmlFileTemplateFormat);       
     }
     }
