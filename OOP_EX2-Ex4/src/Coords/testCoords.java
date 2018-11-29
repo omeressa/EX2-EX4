aa3fa6563a87a5dffe14bc3076833ace296e7a5a
admin <admin@DESKTOP-BUNN6F5>
2018-11-29 14:26:31 +0200

    made test class for testing coords

---
+++ OOP_EX2-Ex4/src/Coords/testCoords.java
@@ -0,0 +1,19 @@
+package Coords;
+
+import Geom.Point3D;
+
+public class testCoords {
+
+	public static void main(String[] args) {
+		MyCoords m = new MyCoords();
+		Point3D p1 = new Point3D(32.103315,35.209039,670);
+		Point3D p2 = new Point3D(32.106352,35.205225,650);
+		System.out.println("distance: "+m.distance3d(p2, p1));
+		Point3D p3 = new Point3D(m.vector3D(p1, p2));
+		System.out.println("vector between: "+m.vector3D(p1, p2));
+		//System.out.println(p3);
+		System.out.println("add vector: "+m.add(p1, p3));
+		System.out.println("cheack: "+m.isValid_GPS_Point(p1));
+	}
+
+}
