5b9fc3fa696603007624f1983d7aeaeb8238829f
admin <admin@DESKTOP-BUNN6F5>
2018-12-02 19:04:24 +0200

    update

--- OOP_EX2-Ex4/src/GIS/GIS_elem.java
+++ OOP_EX2-Ex4/src/GIS/GIS_elem.java
@@ -1,61 +1,71 @@
 package GIS;
 
-
-import Coords.MyCoords;
+import Coords.*;
 import Geom.Geom_element;
 import Geom.Point3D;
 
 
 public class GIS_elem implements GIS_element {
 
-     Geom_element geom;
-     Meta_data data;
-     
-     /**
-      * copy constructor
-      */
-    public GIS_elem(Geom_element geom, Meta_data data) {
-        this.geom = geom;
-        this.data = data;
-    }
-    
-    /**
-     * getGoem function
-     */
-    @Override
-    public Geom_element getGeom() {
-        return geom;
-    }
-    
-    /**
-     * getData function
-     */
-    @Override
-    public Meta_data getData() {
-        return data;
-    }
+	private Point3D point;
+	private GIS_MetDat data;
 
-	@Override
-	public void translate(Point3D vec) {
-		// TODO Auto-generated method stub
-		
+	/**
+	 * gps point information 
+	 *
+	 * @param MAC
+	 * @param SSID
+	 * @param Auth_mode
+	 * @param FS
+	 * @param Channel
+	 * @param RSSI
+	 * @param LAT
+	 * @param LON
+	 * @param ALT
+	 * @param Accuracy
+	 * @param Type
+	 */
+	public GIS_elem(String MAC, String SSID, String Auth_mode, String FS, String Channel, String RSSI,
+			String LAT, String LON, String ALT, String Accurcy, String Type) {
+		String point = "[" + LAT + "," + LON + "," + ALT + "]";
+		this.point = new Point3D(point);
+		//getting the data from MetDat
+		this.data = new GIS_MetDat(MAC, SSID, Auth_mode, FS, Channel, RSSI, Accurcy, Type);
 	}
 
-    /**
-     * tranlase method is to change given input's value
-     * it means to stand at this Gis_element , and to "move" the element by a vector
-     * @param vec is the translated element.
-     */
-    /*
-    @Override
-    public void translate(Point3D vec) {
-        MyCoords myCoords = new MyCoords();
-        GIS_element element = new GIS_elem(vec,this.data);
-        Point3D translate = myCoords.vector3D(new Point3D(element.getGeom().toString()),vec);
-        GIS_element translated = new GIS_elem(translate,element.getData());
-        this.geom = translated.getGeom();
-        this.data = translated.getData();
-    }
-    */
+	public GIS_elem(Point3D geom, Meta_data data2) {
+		this.point = geom;
+		this.data = (GIS_MetDat)data2;
+	}
+
+	@Override
+	public Geom_element getGeom() {
+		return point;
+	}
+
+	@Override
+	public Meta_data getData() {
+		return data;
+	}
+
+	/**
+	 * פונקציית translate:
+	 * לקחת את הנקודה ולהשתמש בפונקציה ממחלקת MYCOORDS 
+	 * של הוספת וקטור
+	 * this is what the other student added on whatsapp for what to do with this function
+	 */
+	@Override
+	public void translate(Point3D vec) {
+		MyCoords coord = new MyCoords();
+		Point3D gps = coord.add(point, vec);
+		point = new Point3D(gps.x(),gps.y(),gps.z());		
+	}
+
+	@Override
+	public String toString() {
+		return "GIS_elem [gps point=" + point + ", data=" + data + "]";
+	}
+
+
 
 }
