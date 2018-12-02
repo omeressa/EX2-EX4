bf67a785e25c1f008d2905a204b42c81e9852652
admin <admin@DESKTOP-BUNN6F5>
2018-12-02 19:42:43 +0200

    update

--- OOP_EX2-Ex4/src/File_format/test.java
+++ OOP_EX2-Ex4/src/File_format/test.java
@@ -6,8 +6,8 @@ public class test {
 	public static void main(String[] args) throws FileNotFoundException {
 
 		
-		String file1 = "Data/data1/WigleWifi_20171201110209.csv";
-		String file2 = "Data/data2/WigleWifi_20171203085618.csv";
+		String file1 = "OOP_EX2-EX4/Data/data1/WigleWifi_20171201110209.csv";
+		String file2 = "OOP_EX2-EX4/Data/data2/WigleWifi_20171203085618.csv";
 		Csv2Kml.turn(file1);
 		Csv2Kml.turn(file2);
 
