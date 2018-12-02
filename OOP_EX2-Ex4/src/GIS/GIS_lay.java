9aac14c3c3251307c2ce14990f4469d7a7bd3e9e
admin <admin@DESKTOP-BUNN6F5>
2018-12-02 19:03:27 +0200

    update

--- OOP_EX2-Ex4/src/GIS/GIS_lay.java
+++ OOP_EX2-Ex4/src/GIS/GIS_lay.java
@@ -1,91 +1,101 @@
 package GIS;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
-import Geom.Point3D;
+/**
+ * in this class we used set that holds all the info
+ */
+public class GIS_lay implements GIS_layer {
 
-public class GIS_lay {
+	private Other gps_point;
+	private Set<GIS_element> set;
+	public String name;
 
-	
-    Set<GIS_element> set = new HashSet<>();
-    Meta_data data;
-    long time;
-	
-    public GIS_lay(){////
-        time = System.currentTimeMillis();
-   }
-    
-    public void setData(long data) {////
-        if (this.data == null) this.data = new GIS_MetDat(data);
-    }
-    
-	public boolean add(GIS_element arg0){////
+
+	public GIS_lay(String filename) {
+		gps_point = new Other();
+		set = new LinkedHashSet<GIS_element>();
+		this.name = filename;
+	}
+
+	@Override
+	public Meta_data get_Meta_data() {
+		return gps_point;
+	}
+
+	public boolean add(GIS_element arg0) {////
 		return set.add(arg0);
 	}
 
-	
-	public boolean addAll(Collection<? extends GIS_element> arg0){////
+
+	public boolean addAll(Collection<? extends GIS_element> arg0) {////
 		return set.addAll(arg0);
 	}
 
-	public void clear(){////
+	public void clear() {////
 		set.clear();
 	}
 
-	public boolean contains(Object arg0){////
+	public boolean contains(Object arg0) {////
 		return set.contains(arg0);
 	}
 
-	public boolean containsAll(Collection<?> arg0){////
+	public boolean containsAll(Collection<?> arg0) {////
 		return set.containsAll(arg0);
 	}
 
-	public boolean isEmpty(){////
+	public boolean isEmpty() {////
 		return set.isEmpty();
 	}
 
-	public Iterator<GIS_element> iterator(){////
+	public Iterator<GIS_element> iterator() {////
 		return set.iterator();
 	}
 
-	public boolean remove(Object arg0){////
+	public boolean remove(Object arg0) {////
 		return set.remove(arg0);
 	}
 
-	public boolean removeAll(Collection<?> arg0){////
+	public boolean removeAll(Collection<?> arg0) {////
 		return set.remove(arg0);
 	}
 
-	public boolean retainAll(Collection<?> arg0){////
+	public boolean retainAll(Collection<?> arg0) {////
 		return set.retainAll(arg0);
 	}
 
-	public int size(){////
+	public int size() {////
 		return set.size();
 	}
 
-	public Object[] toArray(){/////
+	public Object[] toArray() {/////
 		return set.toArray();
 	}
 
-	public <T> T[] toArray(T[] arg0){////
+	public <T> T[] toArray(T[] arg0) {////
 		return set.toArray(arg0);
 	}
-	
-   public Meta_data get_Meta_data(){////
-       return new Meta_data(){
-           @Override
-           public long getUTC(){
-               return time;
-           }
-           @Override
-           public Point3D get_Orientation(){
-               return null;
-           }
-       };
-   }
-	
+
+	/**
+	 * helping function
+	 */
+	public String name() {
+		return this.name;
+	}
+
+	/**
+	 * writing each line in the file
+	 *
+	 * @return data
+	 */
+	public String toString()
+	{
+		StringBuilder SB = new StringBuilder();
+		for (GIS_element line : this.set)
+			SB.append(line.toString()+'\n');//writing data and sorting them!
+		return SB.toString();
+	}
 }
