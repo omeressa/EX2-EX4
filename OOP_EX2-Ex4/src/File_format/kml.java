80953b98bc7b90869e56d42ca18491741428f97d
admin <admin@DESKTOP-BUNN6F5>
2018-12-03 00:31:00 +0200

    test

--- OOP_EX2-Ex4/src/File_format/kml.java
+++ OOP_EX2-Ex4/src/File_format/kml.java
@@ -1,4 +1,3 @@
-
 package File_format;
 
 import GIS.*;
@@ -6,31 +5,45 @@ import Geom.Point3D;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Iterator;
 
 
 public final class kml {
 
-
+	//private final PrintWriter PW;
+	//private final BufferedWriter PW;
 	private Collection<GIS_element> points = new HashSet<>();
 
 
 	public void addLayer(GIS_layer layer) {
 		for (GIS_element element : layer) {
-			points.add(element);
+			points.add( element);
 		}
 	}
+	/*public kml(String output)throws IOException
+	{
+    	 PW = new PrintWriter(new File(output));
+	}*/
+
+
 	/**
-	 * an easy code 
-	 * @param path
-	 * @throws FileNotFoundException
+	 * this function write the Project to KML, it does double iteration, one on the layer, and one on the element
+	 * and writes each element at a time to the kml file.
+	 * the constructor must run before this!
+	 * @param project
+	 * @throws FileNotFoundException 
 	 */
-	public void make(String path) throws FileNotFoundException {
+	public void make(String path) throws FileNotFoundException
+	{
 		PrintWriter PW = new PrintWriter(new File(path));
 		PW.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 		PW.println("<kml xmlns=\"http://www.opengis.net/kml/2.2\">");
@@ -38,19 +51,20 @@ public final class kml {
 		PW.println("<Folder>");
 		PW.println("<name/>");
 		for (GIS_element element : points) {
-			PW.println("<Placemark>");
+					PW.println("<Placemark>\n");
 			PW.println("<name/>");
 			Date date = new Date(element.getData().getUTC());
 			DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			PW.println("<description><![CDATA[Timestamp: <b>" + element.getData().getUTC() + "</b><br/>Date: <b>" + df.format(date) + "</b>]]></description>");
+			PW.println("<name>"+"<![CDATA["+((GIS_elem) element).getMd().getSSID()+"]]>"  +"</name>\n");
+			PW.println("<description>"+"<![CDATA[BSSID: <b>"+((GIS_elem) element).getMd().getMac()+"</b><br/>Capabilities: <b>"+((GIS_elem) element).getMd().getAuth_mode()+"</b><br/>Timestamp: <b>"+((GIS_elem) element).getMd().getRssi()+"</b><br/>Channel: <b>"+((GIS_elem) element).getMd().getChannel()+"</b><br/>RSSI: <b>"+((GIS_elem) element).getMd().getRssi()+"</b><br/>AltitudeMeters: <b>"+((GIS_elem) element).getP().z()+"</b><br/>AccuracyMeters: <b>"+((GIS_elem) element).getMd().getAccuracy()+"</b><br/>Type: <b>"+((GIS_elem) element).getMd().getType()+"</b><br/>Date: <b>"+((GIS_elem) element).getMd().getFirst_seen()+"Timestamp: <b>" + element.getData() + "</b><br/>Date: <b>" + df.format(date)  + "</b>]]></description>");
 			PW.println("<Point>");
 			PW.println("<coordinates>" + ((Point3D) element.getGeom()).y() + "," + ((Point3D) element.getGeom()).x() + "," + ((Point3D) element.getGeom()).z() + "</coordinates>");
-			PW.println("</Point>");
+			PW.println("</Point>\n");
 			PW.println("</Placemark>");
-		}
+			}
+		
 		PW.println("</Folder>");
 		PW.println("</Document>");
 		PW.println("</kml>");
-		PW.close();
+		PW.close();	}
 	}
-}
--- OOP_EX2-Ex4/src/GIS/GIS_elem.java
+++ OOP_EX2-Ex4/src/GIS/GIS_elem.java
@@ -1,71 +1,88 @@
 package GIS;
 
-import Coords.*;
+import Coords.MyCoords;
 import Geom.Geom_element;
 import Geom.Point3D;
 
-
 public class GIS_elem implements GIS_element {
+	GIS_MetDat data ;
+	Point3D point;
+private String arr[];
+	
 
-	private Point3D point;
-	private GIS_MetDat data;
+public GIS_elem(String MAC, String SSID, String Auth_mode, String FS, String Channel, String RSSI,
+String LAT, String LON, String ALT, String Accurcy, String Type) {
+String point = "[" + LAT + "," + LON + "," + ALT + "]";
+this.point = new Point3D(point);
+//getting the data from MetDat
+this.data = new GIS_MetDat(MAC, SSID, Auth_mode, FS, Channel, RSSI, Accurcy, Type);
+}
+public GIS_elem(Point3D geom, Meta_data data2) {
+this.point = geom;
+this.data = (GIS_MetDat)data2;
+}
 
-	/**
-	 * gps point information 
-	 *
-	 * @param MAC
-	 * @param SSID
-	 * @param Auth_mode
-	 * @param FS
-	 * @param Channel
-	 * @param RSSI
-	 * @param LAT
-	 * @param LON
-	 * @param ALT
-	 * @param Accuracy
-	 * @param Type
-	 */
-	public GIS_elem(String MAC, String SSID, String Auth_mode, String FS, String Channel, String RSSI,
-			String LAT, String LON, String ALT, String Accurcy, String Type) {
-		String point = "[" + LAT + "," + LON + "," + ALT + "]";
-		this.point = new Point3D(point);
-		//getting the data from MetDat
-		this.data = new GIS_MetDat(MAC, SSID, Auth_mode, FS, Channel, RSSI, Accurcy, Type);
-	}
+@Override
+public Geom_element getGeom() {
+return point;
+}
 
-	public GIS_elem(Point3D geom, Meta_data data2) {
-		this.point = geom;
-		this.data = (GIS_MetDat)data2;
-	}
+@Override
+public Meta_data getData() {
+return data;
+}
 
-	@Override
-	public Geom_element getGeom() {
-		return point;
-	}
+/**
+* פונקציית translate:
+* לקחת את הנקודה ולהשתמש בפונקציה ממחלקת MYCOORDS 
+* של הוספת וקטור
+* this is what the other student added on whatsapp for what to do with this function
+*/
+@Override
+public void translate(Point3D vec) {
+MyCoords coord = new MyCoords();
+Point3D gps = coord.add(point, vec);
+point = new Point3D(gps.x(),gps.y(),gps.z());		
+}
 
-	@Override
-	public Meta_data getData() {
+@Override
+public String toString() {
+return "GIS_elem [gps point=" + point + ", data=" + data + "]";
+}
+	public GIS_MetDat getMd() {
 		return data;
 	}
 
+	public void setMd(GIS_MetDat md) {
+		this.data = md;
+	}
+
+	public Point3D getP() {
+		return point;
+	}
+
+	public void setP(Point3D p) {
+		this.point = p;
+	}
+
+	public String[] getArr() {
+		return arr;
+	}
+
+	public void setArr(String[] arr) {
+		this.arr = arr;
+	}
+	
 	/**
-	 * פונקציית translate:
-	 * לקחת את הנקודה ולהשתמש בפונקציה ממחלקת MYCOORDS 
-	 * של הוספת וקטור
-	 * this is what the other student added on whatsapp for what to do with this function
+	 * the constractor, get a row from the CSV and split it to the right places
+	 * @param str
 	 */
-	@Override
-	public void translate(Point3D vec) {
-		MyCoords coord = new MyCoords();
-		Point3D gps = coord.add(point, vec);
-		point = new Point3D(gps.x(),gps.y(),gps.z());		
+	public GIS_elem(String str)
+	{
+		arr=str.split(",");
+		point = new Point3D(Double.parseDouble(arr[6]),Double.parseDouble(arr[7]),Double.parseDouble(arr[8]));
+		data = new GIS_MetDat(arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[9],arr[10]);
 	}
-
-	@Override
-	public String toString() {
-		return "GIS_elem [gps point=" + point + ", data=" + data + "]";
-	}
-
 
 
 }
