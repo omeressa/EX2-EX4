936b02b79a08f4c24b853e71b5c602dccbee420f
admin <admin@DESKTOP-BUNN6F5>
2018-12-02 18:59:51 +0200

    update

--- OOP_EX2-Ex4/src/GIS/Other.java
+++ OOP_EX2-Ex4/src/GIS/Other.java
@@ -1,41 +1,33 @@
 package GIS;
 
 import Geom.Point3D;
-
 import java.util.Date;
 
-/**
- * This class implements the metadata interface which allows us to create metadata about the project (TIME OF CREATION)
- */
-public class Other implements Meta_data
-{
-    private long current_time;
 
-    public Other()
-    {
-        Date date = new Date();
-        current_time = date.getTime();
-    }
+public class Other implements Meta_data {
+	private long time;
 
-    @Override
-    public long getUTC()
-    {
-        return current_time;
-    }
+	public Other() {
+		Date date = new Date();
+		time = date.getTime();
+	}
 
-    /**
-     * as proffesor asked, null
-     */
-    @Override
-    public Point3D get_Orientation()
-    {
-        return null;
-    }
+	@Override
+	public long getUTC() {
+		return time;
+	}
 
-    
-    @Override
-    public String toString()
-    {
-        return "time: " + getUTC();
-    }
+	/**
+	 * as proffesor asked, null
+	 */
+	@Override
+	public Point3D get_Orientation() {
+		return null;
+	}
+
+
+	@Override
+	public String toString() {
+		return "time: " + getUTC();
+	}
 }
