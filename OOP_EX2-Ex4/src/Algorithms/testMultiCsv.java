0b6d72959b5a41e46786bff3fd70d7c56dd8ed30
admin <admin@DESKTOP-BUNN6F5>
2018-12-02 20:34:59 +0200

    ..

--- OOP_EX2-Ex4/src/Algorithms/testMultiCsv.java
+++ OOP_EX2-Ex4/src/Algorithms/testMultiCsv.java
@@ -1,5 +1,6 @@
 package Algorithms;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 
 import GIS.GIS_proj;
@@ -9,8 +10,9 @@ public class testMultiCsv {
 
 
 	public static void main(String[] args) throws FileNotFoundException {
-		String files="OOP_EX2-EX4/Data";;
-		GIS_proj project=(GIS_proj) MultiCsv.compine_Csv_File_In_One_Kml(files);
+		File files = new File("OOP_EX2-EX4/Data");
+		String files1="OOP_EX2-EX4/Data";;
+		GIS_proj project=(GIS_proj) MultiCsv.compine_Csv_File_In_One_Kml(files1);
    		
 	}
 }
