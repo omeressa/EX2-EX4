f99bc7f22d6d8c7f9e6400db5c75b8d5887f59f3
admin <admin@DESKTOP-BUNN6F5>
2018-12-02 20:17:27 +0200

    first test

--- OOP_EX2-Ex4/src/Algorithms/MultiCsv.java
+++ OOP_EX2-Ex4/src/Algorithms/MultiCsv.java
@@ -1,5 +1,52 @@
 package Algorithms;
 
+import File_format.kml;
+import GIS.Csv2Gis;
+import GIS.GIS_layer;
+import GIS.GIS_proj;
+import GIS.GIS_project;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class MultiCsv {
 
+    public static  GIS_project creatProject(String file) throws FileNotFoundException {
+        GIS_project project = recursiveSearch(new File(file));
+        kml kml = new kml();
+        for (GIS_layer layer : project) {
+            kml.addLayer(layer);
+        }
+        // save kml as a text file
+        kml.make(file + "/CsvsAsKml.kml");
+        return project;
+    }
+
+    public static GIS_project recursiveSearch(File directory) {
+        GIS_project project = new GIS_proj();
+        if (directory.isDirectory()) {
+            try {
+                File[] files = directory.listFiles();
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        GIS_project tempProject = recursiveSearch(file);
+                        for (GIS_layer layer : tempProject) {
+                            project.add(layer);
+                        }
+                    }
+                    if (isCsv(file)) {
+                        GIS_layer layer = Csv2Gis.csv2gisLayer(file.getAbsolutePath());
+                        project.add(layer);
+                    }
+                }
+            } catch (Exception e) {
+                // unexpected catastrophe
+            }
+        }
+        return project;
+    }
+
+    public static boolean isCsv(File fileType){
+        return fileType.getName().endsWith(".csv");
+    }
 }
--- OOP_EX2-Ex4/src/Algorithms/testMultiCsv.java
+++ OOP_EX2-Ex4/src/Algorithms/testMultiCsv.java
@@ -1,5 +1,21 @@
 package Algorithms;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import GIS.GIS_layer;
+import GIS.GIS_proj;
+import File_format.Csv2Kml;
+
 public class testMultiCsv {
 
+
+	public static void main(String[] args) throws FileNotFoundException {
+        File file=new File("OOP_EX2-EX4/Data");
+		String files="OOP_EX2-EX4/Data";
+		GIS_proj project =  (GIS_proj) MultiCsv.creatProject(files);
+   
+		//Csv2Kml.turn(files);
+		
+	}
 }
